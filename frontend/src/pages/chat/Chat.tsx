75b33509deb4711ccc4e2e3c05b98c6c61bead1b
VijayakannanRaju <vjknnr@gmail.com>
2024-05-27 23:57:28 +0530

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -11,6 +11,8 @@ import DOMPurify from 'dompurify'
 
 import styles from './Chat.module.css'
 import Contoso from '../../assets/Contoso.svg'
+import BP from '../../assets/BP.png'
+
 import { XSSAllowTags } from '../../constants/xssAllowTags'
 
 import {
@@ -751,7 +753,7 @@ const Chat = () => {
           <div className={styles.chatContainer}>
             {!messages || messages.length < 1 ? (
               <Stack className={styles.chatEmptyState}>
-                <img src={ui?.chat_logo ? ui.chat_logo : Contoso} className={styles.chatIcon} aria-hidden="true" />
+                <img src={BP} className={styles.chatIcon} aria-hidden="true" />
                 <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
                 <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
               </Stack>
