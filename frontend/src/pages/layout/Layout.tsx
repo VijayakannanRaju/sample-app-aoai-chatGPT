e898d5453bddf5eb644c6bfa1b1206d3f4a8cdf1
VijayakannanRaju <vjknnr@gmail.com>
2024-05-27 23:26:41 +0530

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -72,7 +72,7 @@ const Layout = () => {
       <header className={styles.header} role={'banner'}>
         <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
           <Stack horizontal verticalAlign="center">
-            <img src={ui?.logo ? ui.logo : BP} className={styles.headerIcon} aria-hidden="true" alt="" />
+            <img src={BP} className={styles.headerIcon} aria-hidden="true" alt="" />
             <Link to="/" className={styles.headerTitleContainer}>
               <h1 className={styles.headerTitle}>{ui?.title}</h1>
             </Link>
