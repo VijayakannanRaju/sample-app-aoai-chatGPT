eec5e791fea298a7b6eb583eaea2e1f105839964
Raju, Vijayakannan (WIPRO) <vijayakannan.raju@bp.com>
2024-06-04 12:07:58 +0530

    pulled the new app.py code from microsoft

--- app.py
+++ app.py
@@ -215,8 +215,8 @@ def prepare_model_args(request_body, request_headers):
         "max_tokens": app_settings.azure_openai.max_tokens,
         "top_p": app_settings.azure_openai.top_p,
         "stop": app_settings.azure_openai.stop_sequence,
-        "stream": app_settings.azure_openai.model,
-        "model": app_settings.azure_openai.stop_sequence,
+        "stream": app_settings.azure_openai.stream,
+        "model": app_settings.azure_openai.model,
         "user": user_json
     }
 
